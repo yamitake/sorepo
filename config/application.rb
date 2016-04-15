886a4a03a685a6b7b96548007e215445d50b18da
jun takeno <take.yapr@gmail.com>
2016-04-15 18:11:59 +0900

    locale setting

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,8 @@ module Sorepo
     config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '**', '*.{rb,yml}').to_s]
+    I18n.enforce_available_locales = false
     config.i18n.default_locale = :ja
 
     config.generators do |g|
