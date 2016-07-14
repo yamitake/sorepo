61afa6a814b8c21adf65ba17fba3d9473f241435
jun takeno <take.yapr@gmail.com>
2016-07-14 17:32:15 +0900

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
                     sign_out: 'logout'
                   },
                 controllers: 
-                  { 
+                  {
                     sessions: 'admin/sessions'
                   }
   namespace :admin do
